265de5bce0642ab084d4322e9ccae2aed5ec80f6
narei_6622 <MikuVick7139@gmail.com>
2025-10-01 08:25:23 +0900

    20251001

--- DynamicObject/DynamicPawnStateController.cs
+++ DynamicObject/DynamicPawnStateController.cs
@@ -12,7 +12,7 @@ namespace RimSpine2DFramework
 {
     public class DynamicPawnStateController
     {
-        private const int VerbRetentionTicks = 120;
+        private const int VerbRetentionTicks = 30;
 
         private static readonly PropertyInfo NeedCurCategoryProperty = AccessTools.Property(typeof(Need), "CurCategory");
         private static readonly PropertyInfo NeedCurLevelCategoryProperty = AccessTools.Property(typeof(Need), "CurLevelCategory");
