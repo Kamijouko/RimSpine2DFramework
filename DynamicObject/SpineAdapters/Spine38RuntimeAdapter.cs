98d396c243f356537f3ab088833d8855c3af5a25
narei_6622 <MikuVick7139@gmail.com>
2025-10-01 21:23:15 +0900

    20251001

--- DynamicObject/SpineAdapters/Spine38RuntimeAdapter.cs
+++ DynamicObject/SpineAdapters/Spine38RuntimeAdapter.cs
@@ -118,7 +118,7 @@ namespace RimSpine2DFramework
             skeleton.transform.localScale = new Vector3(instance.scale.x * config.Scale.x, instance.scale.y * config.Scale.y, instance.scale.z);
             Quaternion baseRotation = instance.def == null ? Quaternion.LookRotation(Vector3.down, Vector3.forward) : Quaternion.identity;
             skeleton.transform.rotation = baseRotation * Quaternion.Euler(config.Rotation);
-            skeleton.transform.localPosition = new Vector3(/*instance.position.x + */config.Offset.x, /*instance.position.y + */config.CameraDistance, /*instance.position.z + */config.Offset.y);
+            skeleton.transform.localPosition = new Vector3(config.Offset.x, config.CameraDistance, config.Offset.y);
             if (!string.IsNullOrEmpty(config.Skin))
             {
                 skeleton.skeleton.SetSkin(config.Skin);
