7b18b29fc70c2f599dd73543de701b0a38858250
narei_6622 <MikuVick7139@gmail.com>
2025-10-01 17:31:59 +0900

    20251001

--- DynamicObject/DynamicObjectInstance.cs
+++ DynamicObject/DynamicObjectInstance.cs
@@ -204,8 +204,8 @@ namespace RimSpine2DFramework
             transform.position = targetPosition;
             position = targetPosition;
 
-            if (curPawn.Rotation == Rot4.East) transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector3.up), 0.2f);
-            if (curPawn.Rotation == Rot4.West) transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector3.down), 0.2f);
+            if (curPawn.Rotation == Rot4.East) transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector3.up), 0.6f);
+            if (curPawn.Rotation == Rot4.West) transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Vector3.forward, Vector3.down), 0.6f);
         }
 
         public void Update()
