563d88096f2cac89b80a854a73041dd9ca03e31f
NazunaRei <59531368+Kamijouko@users.noreply.github.com>
2025-10-05 03:11:43 +0900

    Refactor draw position sync for hidden pawns

--- DynamicObject/DynamicPawnStateRegistry.cs
+++ DynamicObject/DynamicPawnStateRegistry.cs
@@ -197,7 +197,7 @@ namespace RimSpine2DFramework
                     if (bound)
                     {
                         instance.RefreshMapVisibility();
-                        instance.SyncWithPawnPosition();
+                        instance.SyncWithPawnPosition(out _);
                         dObject.transform.position = instance.transform.position;
                     }
                     //Log.Warning("spawned.");
@@ -377,7 +377,7 @@ namespace RimSpine2DFramework
             DynamicPawnStateController controller = new DynamicPawnStateController(instance, pawn, match);
             ControllersByPawn[pawn] = controller;
             ControllersByInstance[instance] = controller;
-            instance.SyncWithPawnPosition();
+            instance.SyncWithPawnPosition(out _);
             controller.RefreshNow();
             return true;
         }
